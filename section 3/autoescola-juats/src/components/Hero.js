2c0a6bf16b511c0452df15a088d5a234a7eeacb3
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2021-12-14 13:57:54 -0300

    GlobalStyle

---
+++ section 3/autoescola-juats/src/components/Hero.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const Root = styled.div`
+    color: #fff;
+
+    ${(props) => css`
+        background: url(${props.image}), rgba(0, 0, 0, 0.4);
+        background-size: cover;
+        background-position: center;
+        background-blend-mode: overlay;
+    `};
+`
+
+const Title = styled.h1`
+    font-weight: 700;
+    letter-spacing: 2px;
+`
+
+const Content = styled.div`
+    p,
+    li {
+        font-size: 20px;
+        font-weight: 300;
+    }
+
+    ul {
+        list-style: none;
+        padding-left: 0;
+    }
+
+    li {
+        &::before {
+            content: '\\2713\\0020';
+        }
+    }
+`
+
+const Hero = ({ image, tittle, children }) => (
+    <Root image={image}>
+        <div>
+            <Title>{tittle}</Title>
+            <Content>{children}</Content>
+        </div>
+    </Root>
+)
+
+export default Hero
