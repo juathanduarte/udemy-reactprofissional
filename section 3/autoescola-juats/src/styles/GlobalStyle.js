3da48f49a8bf770afddcc269c5c2eb107229851c
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2022-01-11 15:33:13 -0300

    1

--- section 3/autoescola-juats/src/styles/GlobalStyle.js
+++ section 3/autoescola-juats/src/styles/GlobalStyle.js
@@ -1,6 +1,8 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
+import { Helmet } from 'react-helmet'
+import { React } from 'react'
 
-import { BreakpointSizes, breakAt } from "./Breakpoints";
+import { BreakpointSizes, breakAt } from './Breakpoints'
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -79,6 +81,18 @@ const GlobalStyle = createGlobalStyle`
       font-size: 1.25rem;
     }
   }
-`;
+`
 
-export default GlobalStyle;
+const GLobalStyleComposed = () => {
+    ;<>
+        <GlobalStyle />
+        <Helmet>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap"
+                rel="stylesheet"
+            />
+        </Helmet>
+    </>
+}
+
+export default GLobalStyleComposed
